a855d306e1708362a3d0eedb1cf5fb6e9905d15d
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-04-18 23:06:23 +0600

    list class is added to store data into master branch

--- To-do-list.cpp
+++ To-do-list.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<list>
+//#include<input>
 
 int main()
 {
---
+++ list.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+//#include<input>
+
+class list{
+public:
+    int dd,mm,yy,cn;
+    string task_name,description;
+};
