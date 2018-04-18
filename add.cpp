4b8418c7b1532dcf313d3ba5e5c30e8c2897744f
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-04-19 00:28:01 +0600

    git add file is added to this branch for add any data into the todo list

---
+++ add.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+bool list::add(){
+    add_out(1);
+    std::cin>>task_name;
+    add_out(0);
+    std::cin>>dd>>mm>>yy;
+    return 1;
+}
--- list.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<string>
-#include<input>
-
-class list{
-public:
-    int dd,mm,yy,cn;
-    string task_name,description;
-};
