8abff150f11ddf5d4649dc8c256206b5f8181f13
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-04-19 02:24:54 +0600

    all sub branches are merged into master branch

---
+++ To-do-list.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "list.cpp"
+#include "show.cpp"
+#include "add.cpp"
+#include "delet.cpp"
+#include "file.cpp"
+
+
+int main(){
+
+
+    return 0;
+}
---
+++ list.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+class list{
+public:
+    int dd,mm,yy,item;
+    std::string task_name,description;
+    bool add();
+    bool show(int task_no);
+    bool delet(int task_no);
+    bool exit();
+}task_list[1005],task;
